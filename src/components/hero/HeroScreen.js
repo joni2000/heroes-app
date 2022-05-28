1107167edd300c335c717cfeeba7aab83b3106ae
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-05-28 20:51:47 -0300

    hice una funcion para traer al heroe segun su id

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useParams, Navigate } from "react-router-dom"
+import { getHeroById } from "../../selectors/getHeroeById";
+
 
 export const HeroScreen = () => {
+
+  const { heroeId } = useParams();
+
+  const hero = getHeroById( heroeId )
+
+  if( !hero ) {
+    return <Navigate to='/' />
+  }
+
   return (
     <div>
         <h1>HeroScreen</h1>
