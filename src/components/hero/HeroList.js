1fd5d6c5a1ab9ed9ce3bf04a1f4e583da428d9bc
Jonathan Ibarrola <ibarrolajonathan@gmail.com>
2022-05-30 16:58:56 -0300

    le di estilos al componente heroScreen

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,10 +1,11 @@
 import { HeroCard } from "./HeroCard";
 
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher"
+import { useMemo } from "react";
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher(publisher);
+    const heroes = useMemo( ()=> getHeroesByPublisher(publisher), [ publisher ]);
 
     return (
         <div className="row rows-cols-1 row-cols-md-3 g-3">
--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,20 +1,60 @@
-import { useParams, Navigate } from "react-router-dom"
+import { useMemo } from "react";
+import { useParams, Navigate, useNavigate } from "react-router-dom";
 import { getHeroById } from "../../selectors/getHeroeById";
 
-
 export const HeroScreen = () => {
-
   const { heroeId } = useParams();
 
-  const hero = getHeroById( heroeId )
+  const hero = useMemo( ()=> getHeroById(heroeId), [ heroeId ]) 
 
-  if( !hero ) {
-    return <Navigate to='/' />
+  const navigate = useNavigate();
+
+  const handleReturn = ()=> {
+      navigate( -1 )
   }
 
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
+  const { id, superhero, publisher, alter_ego, first_appearance, characters } = hero;
+
+  const imgPath = `/assets/${id}.jpg`;
+
   return (
-    <div>
-        <h1>HeroScreen</h1>
+    <div className="row mt-5">
+        <div className="col-4">
+            <img src={imgPath} alt={superhero} className="img-thumbnail" />
+        </div>
+
+        <div className="col-8">
+            <h3>{superhero}</h3>
+            <ul className="list-group list-group-flush">
+
+                <li className="list-group-item">
+                  <b>Alter ego:</b> {alter_ego}
+                </li>
+
+                <li className="list-group-item">
+                  <b>Publisher:</b> {publisher}
+                </li>
+
+                <li className="list-group-item">
+                  <b>first_appearance:</b> {first_appearance}
+                </li>
+
+            </ul>
+
+            <h5 className="mt-5">Characters</h5>
+            <p>{ characters }</p>
+
+            <button
+              className="btn btn-outline-info"
+              onClick={ handleReturn }
+            >
+              Regresar
+            </button>
+        </div>
     </div>
-  )
-}
+  );
+};
